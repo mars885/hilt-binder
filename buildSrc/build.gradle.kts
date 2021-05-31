99f44151bfccd63d78a2dca49dd1ac91a5a13b3f
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2021-05-31 21:13:17 +0300

    Update kotlin gradle plugin to 1.5.10 inside buildSrc module

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -29,5 +29,5 @@ repositories {
 
 dependencies {
     implementation("com.android.tools.build:gradle:4.2.0")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
 }
