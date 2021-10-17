917891fab872377c7f7277da7fdd371a906b16d8
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2021-10-17 23:49:44 +0300

    Update deps

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -29,8 +29,8 @@ object appConfig {
     const val versionCode = 1
     const val versionName = "1.0.0"
 
-    val javaCompatibilityVersion = JavaVersion.VERSION_1_8
-    val kotlinCompatibilityVersion = JavaVersion.VERSION_1_8
+    val javaCompatibilityVersion = JavaVersion.VERSION_11
+    val kotlinCompatibilityVersion = JavaVersion.VERSION_11
 
 }
 
@@ -70,27 +70,27 @@ object publishingConfig {
 
 object versions {
 
-    const val kotlin = "1.5.10" // also in buildSrc build.gradle.kts file
-    const val gradlePlugin = "4.2.0" // also in buildSrc build.gradle.kts file
+    const val kotlin = "1.5.31" // also in buildSrc build.gradle.kts file
+    const val gradlePlugin = "7.0.3" // also in buildSrc build.gradle.kts file
     const val gradleVersionsPlugin = "0.39.0"
-    const val dokkaPlugin = "1.4.32"
-    const val shadowPlugin = "7.0.0"
-    const val appCompat = "1.3.0"
+    const val dokkaPlugin = "1.5.31"
+    const val shadowPlugin = "7.1.0"
+    const val appCompat = "1.3.1"
     const val navigation = "2.3.5"
-    const val fragmentKtx = "1.3.4"
-    const val constraintLayout = "2.0.4"
+    const val fragmentKtx = "1.3.6"
+    const val constraintLayout = "2.1.1"
     const val dagger = "2.36"
-    const val materialComponents = "1.3.0"
+    const val materialComponents = "1.4.0"
     const val apacheCommons = "3.12.0"
     const val javaPoet = "1.13.0"
-    const val kotlinPoet = "1.8.0"
+    const val kotlinPoet = "1.10.1"
     const val incap = "0.3"
     const val autoService = "1.0"
-    const val ksp = "1.5.10-1.0.0-beta01"
+    const val ksp = "1.5.31-1.0.0"
     const val jUnit = "4.13.2"
     const val jUnitExt = "1.1.2"
     const val truth = "1.1.3"
-    const val ktCompileTesting = "1.4.1"
+    const val ktCompileTesting = "1.4.5"
     const val burst = "1.2.0"
 
 }
