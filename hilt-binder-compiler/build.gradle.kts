14363f3ea232f9eee9bdcdda8d332287c5ed3e4b
IUADE0FY <pavlo.rybitskyi@aval.ua>
2024-06-16 21:15:47 +0300

    Fix Java 16 compatibility issue

--- hilt-binder-compiler/build.gradle.kts
+++ hilt-binder-compiler/build.gradle.kts
@@ -22,6 +22,24 @@ plugins {
     shadow()
 }
 
+// See this https://github.com/tschuchortdev/kotlin-compile-testing?tab=readme-ov-file#java-16-compatibility
+if (JavaVersion.current() >= JavaVersion.VERSION_16) {
+    tasks.withType<Test>().all {
+        jvmArgs(
+            "--add-opens=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.jvm=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
+        )
+    }
+}
+
 // Custom configuration used for dependencies that are copied
 // into the library jar instead of adding it as a POM dependency.
 val shadowed: Configuration = configurations.create("shadowed") {
