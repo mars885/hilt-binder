50061087e16a90ad02d84a681712c768a270b198
mars885 <pavlo.rybitskyi@aval.ua>
2024-06-17 11:56:00 +0300

    Bump KotlinPoet to 1.17.0

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -82,7 +82,7 @@ object versions {
     const val materialComponents = "1.12.0"
     const val apacheCommons = "3.14.0"
     const val javaPoet = "1.13.0" // also in buildSrc build.gradle.kts file
-    const val kotlinPoet = "1.12.0"
+    const val kotlinPoet = "1.17.0"
     const val incap = "0.3"
     const val autoService = "1.0"
     const val jUnit = "4.13.2"
