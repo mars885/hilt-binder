dc602ef5751e6ca643e5954b4b5d57b97fec843d
mars885 <pavlo.rybitskyi@aval.ua>
2024-06-18 02:51:04 +0300

    Bump Truth to 1.4.2

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -86,7 +86,7 @@ object versions {
     const val incap = "1.0.0"
     const val autoService = "1.1.1"
     const val jUnit = "4.13.2"
-    const val truth = "1.1.3"
+    const val truth = "1.4.2"
     const val ktCompileTesting = "1.6.0"
     const val testParamInjector = "1.0"
 }
