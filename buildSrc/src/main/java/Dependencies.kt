164ad4f7a950a09c8ea833a0fb369976811216fd
mars885 <pavlo.rybitskyi@aval.ua>
2024-08-03 22:19:46 +0300

    Bump KSP to 1.9.25-1.0.20

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -73,7 +73,7 @@ object versions {
     const val gradleVersionsPlugin = "0.51.0"
     const val dokkaPlugin = "1.9.20"
     const val shadowPlugin = "8.0.0"
-    const val ksp = "1.9.24-1.0.20"
+    const val ksp = "1.9.25-1.0.20"
     const val dagger = "2.51.1"
     const val appCompat = "1.7.0"
     const val navigation = "2.7.7"
