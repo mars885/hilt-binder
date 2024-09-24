5fad1ec53d67b517f8b3d3bf52bf8028aa80b5a9
mars885 <pavlo.rybitskyi@aval.ua>
2024-09-25 02:24:59 +0300

    Bump Detekt Plugin to 1.23.7

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -68,7 +68,7 @@ object versions {
 
     const val kotlin = "2.0.20" // also in buildSrc build.gradle.kts file
     const val androidPlugin = "8.5.2" // also in buildSrc build.gradle.kts file
-    const val detektPlugin = "1.23.6"
+    const val detektPlugin = "1.23.7"
     const val ktlintPlugin = "12.1.1"
     const val gradleVersionsPlugin = "0.51.0"
     const val dokkaPlugin = "1.9.20"
