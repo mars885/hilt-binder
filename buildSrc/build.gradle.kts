9255a82586709509b718084e230e320414fa2e6c
mars885 <pavlo.rybitskyi@aval.ua>
2024-09-25 12:28:51 +0300

    Revert AGP to 8.3.1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -24,7 +24,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:8.5.2")
+    implementation("com.android.tools.build:gradle:8.3.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
     // See https://github.com/google/dagger/issues/3068#issuecomment-999118496
     // for why it should be here.
